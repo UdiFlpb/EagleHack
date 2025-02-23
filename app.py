1ac5d7856c0cdf52517d1beca72f1b034e009323
AuthentifiAI-Udi <udi@authentifi.ai>
2025-02-22 20:36:59 -0500

    Added support for running the ML file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-
+import subprocess
 
 app = Flask(__name__)
 
@@ -14,13 +14,21 @@ def submit():
     print("Received data:", data)
     
     #run the script here and get the answer:
+    result = subprocess.run(
+        ["python", "HAPred.py", "--male", "1", "--age", "1", "--education", "1", "--currentSmoker", "0", "--cigsPerDay", "0", "--BPMeds", "0", "--prevalentStroke", "0", "--prevalentHyp", "0", "--diabetes", "0", "--totChol", "0", "--sysBP", "0", "--diaBP", "0", "--BMI", "0", "--heartRate", "0", "--glucose", "0"],
+        capture_output=True, 
+        text=True
+    )
     
-    
-    #yes response
-    return jsonify({"message": "Form submitted successfully", "data": "<p><strong>We are 85% sure that you will have a heart attack.\nConsider talking to a doctor.</strong></p>"})
+    if(result.stdout.__contains__("yes")):
+        #yes response
+        return jsonify({"message": "Form submitted successfully", "data": "<p><strong>We are 85% sure that you will have a heart attack.\nConsider talking to a doctor.</strong></p>"})
 
-    #no response
-    return jsonify({"message": "Form submitted successfully", "data": "<p><strong>We are 85% sure that you will not have a heart attack.</strong></p>"})
+    if(result.stdout.__contains__("no")):
+        #no response
+        return jsonify({"message": "Form submitted successfully", "data": "<p><strong>We are 85% sure that you will not have a heart attack.</strong></p>"})
+    else:
+        return jsonify({"message": "Form submitted successfully", "data": "<p><strong>Problem accured.</strong></p>"})
 
     
 if __name__ == '__main__':
